2ef512d0750d5bcc6b2148b120c985b79e25d566
Deepak Kumar <deepak.kumar@girnarsoft.co.in>
2021-12-11 23:23:53 +0530

    xf

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const io=require('socket.io')(3002,{
+    cors:{
+        origin:["http://localhost:3000"]
+    },
+})
+
+io.on('connection',socket=>{
+    console.log(socket.id)
+    socket.on('send',(message)=>{
+        socket.broadcast.emit('recieve',message)
+    })
+})
